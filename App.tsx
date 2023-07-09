55ee9dc6f302d04d160ecceea331280119c7aeaa
WilliamFaller <mew.pvp10@gmail.com>
2023-07-09 13:20:05 -0300

    Configurações inicias do APP

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 import Groups from '@screens/Groups';
+import { Loading } from '@components/Loading';
+import theme from './src/theme';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"  
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading/>}
+    </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
 
 export default function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
