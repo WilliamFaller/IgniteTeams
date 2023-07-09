596740336d1fe92dcbb71822644ab3914981e1be
WilliamFaller <mew.pvp10@gmail.com>
2023-07-09 13:45:28 -0300

    Componente Header Criado

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Header } from '@components/Header'
 
 import { Container } from './styles';
 
 export default function Groups() {
   return (
     <Container>
+      <Header/>
     </Container>
   );
 }
