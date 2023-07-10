57c71e4d2f438a412798ee0628baeccdbbdf49cb
WilliamFaller <mew.pvp10@gmail.com>
2023-07-09 22:47:17 -0300

    Componentes de cabecalho, lista e botao

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
 import { Header } from '@components/Header'
+import { FlatList } from 'react-native';
 
 import { Container } from './styles';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header/>
+      <Highlight title='Turmas' subtitle='jogue com a sua turma'/>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+        contentContainerStyle={ groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma ?'/>}
+      />
+      <Button title={'Criar nova turma'}/>
     </Container>
   );
 }
