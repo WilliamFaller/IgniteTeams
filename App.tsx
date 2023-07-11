3e3f20892144c9f47cb3dea8e78a210f5baa1222
WilliamFaller <mew.pvp10@gmail.com>
2023-07-11 20:57:43 -0300

    Finalizando a tela de Players

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components/native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Players } from '@screens/Players';
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -8,11 +8,14 @@ import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Players() {
 
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState(['Rodrigo', 'Pedro', 'Vini'])
+  const [players, setPlayers] = useState(['Rodrigo', 'Pedro', 'Vini', 'Matheus', 'Ana', 'Maria', 'João', 'José'])
 
   return (
     <Container>
@@ -49,8 +52,26 @@ export function Players() {
           {players.length}
         </NumbersOfPlayers>
       </HeaderList>
-
-
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard 
+            name={item} 
+            onRemove={() => console.log('remover ' + item)}
+          />
+        )}
+        ListEmptyComponent={() => <ListEmpty message='Não há pessoas nesse time!' />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100},
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+      <Button 
+        title="Remover Turma"
+        type="SECONDARY"
+      />
     </Container>
   );
 }
