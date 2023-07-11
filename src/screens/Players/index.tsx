2eb7abb2a54881618b39961eaeb8e1479a3a1442
WilliamFaller <mew.pvp10@gmail.com>
2023-07-10 22:23:02 -0300

    Filtro de turma selecionada

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,19 @@
-import { Container, Form } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
 
 export function Players() {
+
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState(['Rodrigo', 'Pedro', 'Vini'])
+
   return (
     <Container>
       <Header showBackButton/>
@@ -23,6 +31,26 @@ export function Players() {
 
         />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter 
+            title={item}
+            isActived={item === team}   
+            onPress={() => setTeam(item)}        
+          />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
+
+
     </Container>
   );
 }
