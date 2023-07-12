9d4bc0e120cd1bfb5bede862280e64d578780f01
WilliamFaller <mew.pvp10@gmail.com>
2023-07-11 22:10:30 -0300

    Iniciando as configurações de navegação

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Players } from '@screens/Players';
 import { Loading } from '@components/Loading';
 import theme from './src/theme';
+import { Routes } from './src/routes';
 
 
 export default function App() {
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"  
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading/>}
+      {fontsLoaded ? <Routes /> : <Loading/>}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { Header } from '@components/Header'
 import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 import { Container } from './styles';
+
+import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
@@ -10,6 +12,11 @@ import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -26,7 +33,10 @@ export function Groups() {
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma ?' />}
       />
-      <Button title={'Criar nova turma'} />
+      <Button
+        title={'Criar nova turma'}
+        onPress={handleNewGroup}
+      />
     </Container>
   );
 }
